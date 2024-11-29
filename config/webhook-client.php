9eeb824254720e833307c35716dfe4e2fc357e1f
DanielCriollo <danielcriollo9706@gmail.com>
2024-11-29 01:26:39 -0500

    Signature Name

--- config/webhook-client.php
+++ config/webhook-client.php
@@ -20,7 +20,7 @@ return [
             /*
              * The name of the header containing the signature.
              */
-            'signature_header_name' => 'X-Custom-Signature',
+            'signature_header_name' => 'Signature',
 
             /*
              *  This class will verify that the content of the signature header is valid.
